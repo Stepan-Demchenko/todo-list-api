5f1f9bedfe6cc2761eefa01a0971eba19c92ca9d
Stepan Demchenko <stepan.demchenko@provenbase.com>
2024-05-06 18:08:40 +0300

    using env file for the db variables setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodosModule } from './todos/todos.module';
-import { Todo } from "./todos/entities/todo.entity";
+import { Todo } from './todos/entities/todo.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'db',
-      port: 3307,
-      username: 'dev',
-      password: 'dev-123',
-      database: 'todo_list_api',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE_NAME,
       entities: [Todo],
       synchronize: true,
     }),
